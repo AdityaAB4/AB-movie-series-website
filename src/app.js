7f35c6859eaafce9bf910f80e7644c150b3910d5
Aditya Barde <adityabarde4ab@gmail.com>
2020-11-17 15:43:22 +0530

    Updating the code

--- src/app.js
+++ src/app.js
@@ -3,22 +3,13 @@ const { response } = require("express");
 const express = require("express");
 const request = require("request");
 const movname = require("./utils/movname");
-const url = "http://www.omdbapi.com/?s=bjk&apikey=9fba416a";
+// const url = "http://www.omdbapi.com/?s=bjk&apikey=9fba416a";
 
-const name = process.argv[2];
-console.log(process.argv);
+// const name = process.argv[2];
+// console.log(process.argv);
 
-if (!name) {
-  return console.log("Please provide name to search the Movie or Series!");
-} else {
-  movname(name, (error, data) => {
-    if (error) {
-      return console.log(error);
-    } else {
-      console.log(data);
-    }
-  });
-}
+// 
+
 
 // request({ url: url, json: true }, (error, response) => {
 //   if (error) {
@@ -28,10 +19,10 @@ if (!name) {
 //   }
 // });
 
-// const port = 80;
+// // const port = 80;
 const path = require("path");
-const { RSA_NO_PADDING } = require("constants");
-const { title } = require("process");
+// const { RSA_NO_PADDING } = require("constants");
+// const { title } = require("process");
 
 // console.log(__dirname);
 // console.log(path.join(__dirname, "../public"));
@@ -77,22 +68,22 @@ app.get("/home", (req, res) => {
   // })
 
 
-app.get("/products", (req, res) => {
-  if (!req.query.search) {
-    return res.send({
-      error: "You must provide a search term!",
-    });
-  }
-  console.log(req.query.search);
-  res.send({
-    products: [],
-  });
-});
+// app.get("/products", (req, res) => {
+//   if (!req.query.search) {
+//     return res.send({
+//       error: "You must provide a search term!",
+//     });
+//   }
+//   console.log(req.query.search);
+//   res.send({
+//     products: [],
+//   });
+// });
 
 app.get("*", (req, res) => {
   res.send("Oops!!! 404 Error....:(");
 });
 
 app.listen(port, () => {
-  console.log(`The application started successfully on port${port}`);
+  console.log(`The application started successfully on port ${port}`);
 });
